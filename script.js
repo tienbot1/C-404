ab63e0741f0a47233db0d6f2a9ba9d3f8083df0c
tienbot1 <117821070+tienbot1@users.noreply.github.com>
2023-07-06 01:15:36 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ let videoElement = document.getElementById('error-video');
 let imgElement = document.getElementById('error-img');
 
 let getVideo = function(){
-    let numb = Math.floor(Math.random() * 5) + 1;
-    videoElement.src = './videos/' + numb + '.mp4';
+    let numb = Math.floor(Math.random() * 9) + 1;
+    videoElement.src = './videos/' + 9 + '.mp4';
     window.addEventListener('DOMContentLoaded', function() {
         videoElement.width = screenWidth;  
     })
 };
 
 let getImg = function(){
-    let numb = Math.floor(Math.random() * 3) + 1;
+    let numb = Math.floor(Math.random() * 2) + 1;
     imgElement.src = './img/' + numb + '.jpg';
     window.addEventListener('DOMContentLoaded', function() {
         imgElement.width = screenWidth;  
